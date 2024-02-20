d62a9b686597e1c49aa685b584575d2ee909060c
komayuta <customlife.komayuta@gmail.com>
2024-02-20 15:17:44 +0900

    fix

--- index.js
+++
@@ -1,5 +0,0 @@
-const commonParts = (a, b)=>{
-    return console.log(a + b);
-}
-
-module.exports = commonParts;
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export const commonParts = (a: number, b: number):void=>{
+    return console.log(a + b);
+}
+
